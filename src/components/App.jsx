23391d21e745bd03c24c88cc2bfa086c2fb1caf3
Stanislav2022 <tsv5@ukr.net>
2022-10-09 14:35:02 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,16 @@
-import Phonebook from "./Phonebook/Phonebook";
-
 export const App = () => {
   return (
-    <div>
-      <Phonebook/>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
     </div>
   );
 };
